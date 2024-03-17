e2fbf6d56e4a0c0cbf66de86b8b9c0b1cf44abf8
bhavneetkaurraheja <banirahejax@gmail.com>
2024-03-17 21:11:19 +0000

    implementing service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,15 +1,6 @@
 var cacheName = "afterschool-v1";
-var cacheFiles = ["index.html"];
+var cacheFiles = ["index.html","afterschool.webmanifest"]
 
-// self.addEventListener("install", function(e) {
-//     console.log("[Service Worker] Install");
-//     e.waitUntil(
-//         caches.open(cacheName). then(function(cache) {
-//             console.log("[Service Worker] Caching files");
-//             return cache.addAll(cacheFiles);
-//         })
-//     );
-// });
 
 self.addEventListener("install", function (e) {
     console.log("[Service Worker] Install");
@@ -21,33 +12,6 @@ self.addEventListener("install", function (e) {
     );
 });
 
-// self.addEventListener("fetch", function (e) {
-//     e.respondWith(
-//         caches.match(e.request).then(function (cachedFile) {
-//             if (cachedFile) {
-//                 console.log("[Service Worker] Resource fetched from the cache for: " + e.request.url);
-//                 return cachedFile;
-//             } else {
-//                 return fetch(e.request).then(function (response) {
-//                     // Check if the response is cloneable
-//                     if (!response || !response.clone) {
-//                         console.log("[Service Worker] Cannot clone response for: " + e.request.url);
-//                         return response;
-//                     }
-
-//                     return caches.open(cacheName).then(function (cache) {
-//                         // Add new file to the cache
-//                         cache.put(e.request, response.clone());
-
-//                         console.log("[Service Worker] Resource fetched and saved in the cache for: " +
-//                             e.request.url);
-//                         return response;
-//                     });
-//                 });
-//             }
-//         })
-//     );
-// });
 
 self.addEventListener("fetch", function (e) {
     e.respondWith(
--- src/main.js
+++ src/main.js
@@ -1,19 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
 
-// // Service Worker Registration
-// if ('serviceWorker' in navigator) {
-//   navigator.serviceWorker.register('./service-worker.js')
-//     .then((registration) => {
-//       console.log('Service Worker registered with scope:', registration.scope);
-//     })
-//     .catch((error) => {
-//       console.error('Service Worker registration failed:', error);
-//     });
-// }
 
 if ("serviceWorker" in navigator) {
-  navigator.serviceWorker.register("./service-worker.js");
+    navigator.serviceWorker.register("./service-worker.js");
   }
 
 
