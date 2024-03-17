c11c3bb7b30b74ba2a27125c0f186b818e106e69
bhavneetkaurraheja <banirahejax@gmail.com>
2024-03-17 20:52:22 +0000

    dist new version

--- service-worker.js
+++ service-worker.js
@@ -1,38 +1,74 @@
 var cacheName = "afterschool-v1";
-var cacheFiles = [
-    "index.html",
-];
+var cacheFiles = ["index.html"];
 
-self.addEventListener("install", function(e) {
+// self.addEventListener("install", function(e) {
+//     console.log("[Service Worker] Install");
+//     e.waitUntil(
+//         caches.open(cacheName). then(function(cache) {
+//             console.log("[Service Worker] Caching files");
+//             return cache.addAll(cacheFiles);
+//         })
+//     );
+// });
+
+self.addEventListener("install", function (e) {
     console.log("[Service Worker] Install");
     e.waitUntil(
-        caches.open(cacheName). then(function(cache) {
+        caches.open(cacheName).then(function (cache) {
             console.log("[Service Worker] Caching files");
             return cache.addAll(cacheFiles);
         })
     );
 });
 
+// self.addEventListener("fetch", function (e) {
+//     e.respondWith(
+//         caches.match(e.request).then(function (cachedFile) {
+//             if (cachedFile) {
+//                 console.log("[Service Worker] Resource fetched from the cache for: " + e.request.url);
+//                 return cachedFile;
+//             } else {
+//                 return fetch(e.request).then(function (response) {
+//                     // Check if the response is cloneable
+//                     if (!response || !response.clone) {
+//                         console.log("[Service Worker] Cannot clone response for: " + e.request.url);
+//                         return response;
+//                     }
+
+//                     return caches.open(cacheName).then(function (cache) {
+//                         // Add new file to the cache
+//                         cache.put(e.request, response.clone());
+
+//                         console.log("[Service Worker] Resource fetched and saved in the cache for: " +
+//                             e.request.url);
+//                         return response;
+//                     });
+//                 });
+//             }
+//         })
+//     );
+// });
+
 self.addEventListener("fetch", function (e) {
     e.respondWith(
         caches.match(e.request).then(function (cachedFile) {
+            //if the file is in the cache, retrieve it from there
             if (cachedFile) {
-                console.log("[Service Worker] Resource fetched from the cache for: " + e.request.url);
+                console.log(
+                    "[Service Worker] Resource fetched from the cache for: " +
+                    e.request.url
+                );
                 return cachedFile;
             } else {
+                //if the file is not in the cache, download the file
                 return fetch(e.request).then(function (response) {
-                    // Check if the response is cloneable
-                    if (!response || !response.clone) {
-                        console.log("[Service Worker] Cannot clone response for: " + e.request.url);
-                        return response;
-                    }
-
                     return caches.open(cacheName).then(function (cache) {
-                        // Add new file to the cache
+                        //add the new file to the cache
                         cache.put(e.request, response.clone());
-
-                        console.log("[Service Worker] Resource fetched and saved in the cache for: " +
-                            e.request.url);
+                        console.log(
+                            "[Service Worker] Resource fetched and saved in the cache for: " +
+                            e.request.url
+                        );
                         return response;
                     });
                 });
